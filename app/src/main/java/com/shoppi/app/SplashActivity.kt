ac5b8b551a30394b7de8bbd4be72734a85d0ddca
hiharim <kyj96604@gmail.com>
2024-01-18 18:42:29 +0900

    [섹션5] Splash 화면 추가 및 앱 호환성 대응 & Fragment 추가 및 화면 연결

--- app/src/main/java/com/shoppi/app/SplashActivity.kt
+++ app/src/main/java/com/shoppi/app/SplashActivity.kt
@@ -1,11 +1,15 @@
 package com.shoppi.app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
